781bf857143977e43a8c9f7353787eb86ede4533
Boren Zhang <bobo1993324@gmail.com>
2014-08-01 19:20:05 +0800

    update framework and version

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 mkdir package/pdf.js/ -p
-cp qml logo.png manifest.json pdfjsViewer.desktop pdfjsViewer.json pdfjsViewer-contenthub.json Intro.pdf package -rf
+cp qml logo.png manifest.json pdfjsViewer.desktop pdfjsViewer.json pdfjsViewer-contenthub.json package -rf
 cp pdf.js/build/ package/pdf.js -rf
 cd package
 click build .
