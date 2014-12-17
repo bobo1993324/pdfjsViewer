f94b152e5f8dc610d15905be31972312337972db
Boren Zhang <bobo1993324@gmail.com>
2014-12-17 15:53:21 -0500

    add thumbnails

---
+++ www/web/1.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+    $("#sidebarContainer").height(document.body.scrollHeight - 75);
+    $("#navigation-button").click(function() {
+        console.log("navigation button clicked.");
+        if ($("#navigation-list").css('display') == 'none') {
+            $("#navigation-list").show(100);
+        } else {
+            $("#navigation-list").hide(100);
+        }
+    });
+    $("#show-thumbnails-button").click(function(){
+        $("#outerContainer").addClass('sidebarMoving');
+        $("#outerContainer").addClass('sidebarOpen');
+        PDFView.sidebarOpen = true;
+        PDFView.renderHighestPriority();
+        PDFView.switchSidebarView('thumbs');
+        $("#navigation-list").hide(100);
+    });
+    $("#close-sidebar-button").click(function() {
+        this.classList.toggle('toggled');
+        $("#outerContainer").addClass('sidebarMoving');
+        $("#outerContainer").removeClass('sidebarOpen');
+        PDFView.sidebarOpen = false;
+        PDFView.renderHighestPriority();
+    });
+});
